d9e464cb54846dcce67ea820edf5ccd1ec4d9d91
kobywongchunkit <kobywongchunkit@gmail.com>
2021-12-14 17:21:09 +0800

    [todo][koby] rename doneTodo to toggleTodo

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,14 +3,14 @@ import { REMOVE_TODO, TOGGLE_TODO } from '../constants/constants'
 
 function TodoItem({todo}){
     const dispatch = useDispatch();
-    function doneTodo(){
+    function toggleTodo(){
         dispatch({type:TOGGLE_TODO,payload: todo.id})
     }
     function removeTodo(){
         dispatch({type:REMOVE_TODO,payload: todo.id})
     }
     return(
-            <p className={todo.done ?'todoItem done': 'todoItem undone'} onClick={doneTodo}         
+            <p className={todo.done ?'todoItem done': 'todoItem undone'} onClick={toggleTodo}         
               >{todo.text}<span className='cross' onClick={removeTodo}>❌</span></p>
     );
 }
