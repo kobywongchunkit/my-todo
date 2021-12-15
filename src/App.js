ae7fba4873d6969ff215e8256a873b3b3ccfbd55
kobywongchunkit <kobywongchunkit@gmail.com>
2021-12-15 11:42:56 +0800

    [todo][koby] Add basic nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,60 @@
 import TodoList from './components/TodoList';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <TodoList></TodoList>
-    </div>
+    <Router>
+      <div>
+        <ul id="menu">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/Done">Done</Link>
+          </li>
+          <li>
+            <Link to="/Undone">Undone</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/Done">
+            <Done />
+          </Route>
+          <Route path="/Undone">
+            <Undone />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return  <>
+    <div className="App">
+    <TodoList></TodoList>
+  </div></>
+}
+
+function Done() {
+  return <h2>Done</h2>;
+}
+
+function Undone() {
+  return <h2>Undone</h2>;
 }
 
 export default App;
